09a036758fb5bd59199cf0e85a8eb2e2078fb4aa
Reilly Wood <reilly.wood@icloud.com>
2021-10-04 12:19:33 -0700

    Add project files.

---
+++ nfzf/fzf.cs
@@ -0,0 +1,12 @@
+﻿namespace nfzf
+{
+
+
+    // Result contains the results of running a match function.
+    public record Result(int Start, int End, int Score);
+
+    public static class fzf
+    {
+
+    }
+}
